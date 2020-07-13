ba95aa0ec79046f7c1b847db672552072e8cdc72
in5tillMotion <waifusonmymind@gmail.com>
2020-07-14 03:48:45 +0400

    add_reflexia

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import somepackage.Other;
 import somepackage.Some;
+import somethinggreen.AnnotationForExaple;
 import somethinggreen.Contract;
 
 import java.util.ArrayList;
@@ -9,5 +10,26 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        Other.saySomthing();
+        Some some = new Some("Секрет");
+        System.out.println(some.getДаТакТожеМожно());
+        System.out.println(some);
+        some.setNoMoreSecrets(some.getДаТакТожеМожно());
+        System.out.println(some.getSecret(true));
+        System.out.println(some);
+        some.soTellMeAboutInterfaces();
+        System.out.println();
+
+        AnnotationForExaple annotation = Some.class.getAnnotation(AnnotationForExaple.class);
+        System.out.println(annotation.description());
+
+        try {
+            String defaultValue = (String) AnnotationForExaple.class
+                    .getDeclaredMethod("description").getDefaultValue();
+            System.out.println(defaultValue);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
     }
+}
